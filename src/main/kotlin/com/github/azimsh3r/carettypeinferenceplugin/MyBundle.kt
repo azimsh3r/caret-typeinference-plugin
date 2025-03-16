f70fb98c1f3a441a2ddaa29049153494fc6f365e
GitHub Action <action@github.com>
2025-03-16 15:44:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/azimsh3r/carettypeinferenceplugin/MyBundle.kt
+++ src/main/kotlin/com/github/azimsh3r/carettypeinferenceplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.azimsh3r.carettypeinferenceplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/azimsh3r/carettypeinferenceplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/azimsh3r/carettypeinferenceplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.azimsh3r.carettypeinferenceplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.azimsh3r.carettypeinferenceplugin.MyBundle
+import com.github.azimsh3r.carettypeinferenceplugin.services.MyProjectService
 import javax.swing.JButton
 
 
